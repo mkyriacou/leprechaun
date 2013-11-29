fdb30cfe5345662a313234d4855092aff6ee145d
mkyriacou <strattica@gmail.com>
2013-11-29 14:52:34 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Leprechaun::Application.config.secret_token = '8819a786e4991e954a75ab296cf039fd497633611b480a28c61ebde267eba8e16e20be6b7e678ddeb7bccc0814a7e8636ff3ecc4b414e48d0049fb26fc579e63'
